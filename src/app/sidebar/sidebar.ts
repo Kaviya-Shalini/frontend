c7b147bc13a75cd5cc81ab53c64421485f6e4501
Kaviya-Shalini <shalinikaviya5@gmail.com>
2025-10-31 21:04:41 +0530

    update commit

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -14,6 +14,7 @@ export class SidebarComponent implements OnInit {
   isClosed = false;
 
   @Output() sidebarToggle = new EventEmitter<boolean>();
+  //sdfadf
 
   ngOnInit() {
     this.checkScreenSize();
